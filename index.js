abf920eff0c44fa9eff6abead28f380d3b5fadb2
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-05-17 22:12:26 +0600

    server side created

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+const chef = require('./data/data.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+
+});
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
+
+app.get("/chef/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chef.find(n => n.id === id);
+    res.send(selectedChef);
+  });
+
+app.listen(port, () => {
+    console.log(`API is running on port: ${port}`)
+})
